1051a5c91d47796dd131633b2efdf3073f66f4d4
criz <chrastophe@gmail.com>
2024-12-05 14:35:53 +0100

    test

--- src/Plugin/war40kv10/Structure.php
+++ src/Plugin/war40kv10/Structure.php
@@ -42,8 +42,19 @@ class Structure
 
     public function structureKeyWord():array {
         return [
-            ['name'=>'','designation'=>'','categorie'=>'keyWordProfil'],
-            ['name'=>'','designation'=>'','categorie'=>'keywordWeapon']
+            ['name'=>'Infanterie','designation'=>'','categorie'=>'keyWordProfil'],
+            ['name'=>'Vol','designation'=>'','categorie'=>'keyWordProfil'],
+            ['name'=>'Personnage','designation'=>'','categorie'=>'keyWordProfil'],
+            ['name'=>'Héros Épique','designation'=>'','categorie'=>'keyWordProfil'],
+            ['name'=>'Exo-armure','designation'=>'','categorie'=>'keyWordProfil'],
+            ['name'=>'Grenades','designation'=>'','categorie'=>'keyWordProfil'],
+            ['name'=>'Désignateur Laser','designation'=>'','categorie'=>'keyWordProfil'],
+            ['name'=>'Équipe de Cibleurs','designation'=>'','categorie'=>'keyWordProfil'],
+            ['name'=>'Véhicule','designation'=>'','categorie'=>'keyWordProfil'],
+            ['name'=>'Marcheur','designation'=>'','categorie'=>'keyWordProfil'],
+            ['name'=>'ASSAUT','designation'=>'On peut tirer même si l’unité du porteur a Avancé.','categorie'=>'keywordWeapon'],
+            ['name'=>'PISTOLET','designation'=>'On peut tirer même si l’unité du porteur est à Portée d’Engagement d’unités ennemies, mais il faut alors cibler une de ces unités ennemies.On ne peut pas tirer avec des Pistolets et avec d’autres armes n’étant pas des Pistolets (sauf s’il s’agit d’un Monstre ou d’un Véhicule).','categorie'=>'keywordWeapon'],
+            ['name'=>'TIR RAPIDE','designation'=>'[TIR RAPIDE X] : On augmente les Attaques de “x” quand on cible des unités à mi-portée.','categorie'=>'keywordWeapon']
         ];
     }
 }
